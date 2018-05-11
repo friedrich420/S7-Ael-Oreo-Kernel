2d3d3b70c3ddcdf1953d054ce63940b4ecd1c199
EchoeGit <kelpidorou@yahoo.com>
2018-05-11 11:05:03 +0300

    Exynos Mali voltage control and update power tables on actor voltage change

--- drivers/cpufreq/exynos-mp-cpufreq.c
+++ drivers/cpufreq/exynos-mp-cpufreq.c
@@ -26,6 +26,7 @@
 #include <linux/reboot.h>
 #include <linux/delay.h>
 #include <linux/cpu.h>
+#include <linux/ipa.h>
 #include <linux/pm_qos.h>
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
@@ -1691,6 +1692,8 @@ static ssize_t store_volt_table(struct kobject *kobj, struct attribute *attr,
 	}
 
 	mutex_unlock(&cpufreq_lock);
+
+	ipa_update();
 
 	return count;
 }
